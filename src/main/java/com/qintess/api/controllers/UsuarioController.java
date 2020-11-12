a749215aa232976d472ee506f0e76edcbc5d850f
Crusher <Crusher@DESKTOP-673IU8M>
2020-11-12 20:12:38 -0300

    resolvendo bug em usuarioController

--- src/main/java/com/qintess/api/controllers/UsuarioController.java
+++ src/main/java/com/qintess/api/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,8 +53,15 @@ public class UsuarioController {
 			return ResponseEntity.badRequest().body(result.getAllErrors());
 		}
 		
+		var senhaCripto = encodaSenha(usuario.getSenha());
+		usuario.setSenha(senhaCripto);
+		
 		repo.save(usuario);
 		
 		return ResponseEntity.ok(usuario);
+	}
+	
+	private String encodaSenha(String senha) {
+		return new BCryptPasswordEncoder().encode(senha);
 	}
 }
